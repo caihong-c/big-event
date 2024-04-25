0d32a1b3250c6ccc991eee5ea29ccb406699f185
cai <2652411941@qq.com>
2024-04-25 14:29:33 +0800

    add 登陆访问拦截，路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useUserStore } from '@/stores'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,17 @@ const router = createRouter({
       ]
     }
   ]
+})
+
+//设置路由导航守卫
+router.beforeEach((to) => {
+  // to要去的页面 from从哪里来
+  // 如果没有token，且访问的是非登陆页，拦截到登陆，其他页面正常放行
+  const useUser = useUserStore()
+  if (!useUser.token && to.path !== '/login') {
+    return '/login'
+  }
+  return true
 })
 
 export default router
